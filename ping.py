feed0259b4065b197d32907387bf1926408038cd
Tanel Peep <tanel.peep@outlook.com>
2017-12-07 16:51:44 +0200

    packet create and checksum fix

--- ping.py
+++ ping.py
@@ -6,7 +6,10 @@ import functools
 import socket
 from aioconsole import ainput
 import numpy as np
+import select
+import async_timeout
 import random
+import os
 
 
 ICMP_ECHO_REQUEST = 8
@@ -26,11 +29,35 @@ else:
 class PingPacket(object):
     def __init__(self, packet_seq):
 
-        self.buffer = 0  # type: np.ndarray
+        self.buffer = 0
         self.packet = 0
         self.checksum = 0
         self.packet_seq = packet_seq
         self.create_packet()
+
+    @staticmethod
+    def create_checksum(buffer):
+
+        # I'm not too confident that this is right but testing seems to
+        # suggest that it gives the same answers as in_cksum in ping.c.
+        sum = 0
+        count_to = (len(buffer) / 2) * 2
+        count = 0
+        while count < count_to:
+            this_val = ord(buffer[count + 1]) * 256 + ord(buffer[count])
+            sum = sum + this_val
+            sum = sum & 0xffffffff  # Necessary?
+            count = count + 2
+        if count_to < len(buffer):
+            sum = sum + ord(buffer[len(buffer) - 1])
+            sum = sum & 0xffffffff  # Necessary?
+        sum = (sum >> 16) + (sum & 0xffff)
+        sum = sum + (sum >> 16)
+        answer = ~sum
+        answer = answer & 0xffff
+        # Swap bytes. Bugger me if I know why.
+        answer = answer >> 8 | (answer << 8 & 0xff00)
+        return answer
 
     def create_packet(self):
         icmp_type = ICMP_ECHO_REQUEST
@@ -39,48 +66,29 @@ class PingPacket(object):
         my_checksum = 0
 
         # Make a dummy header with a 0 checksum.
-        header = struct.pack("BbHHh", icmp_type, 0, my_checksum, self.packet_seq, 1)
-        bytes_in_double = struct.calcsize("d")
-        data = "Testin"
-        data = struct.pack("d", default_timer()) + data.encode("ascii")
+        # Header is type (8), code (8), checksum (16), id (16), sequence (16)
+        packet_id = int(1)
+        header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0, 0, packet_id, 1)
+        #bytes_in_double = struct.calcsize("d")
+
+        data = 192 * 'Q'
+        #data = struct.pack("d", default_timer()) + data.encode("ascii")
+        #buffer = header + data
 
         # Calculate the checksum on the data and the dummy header.
-        self.buffer = header + data
-        self.create_checksum()
-        my_checksum = self.checksum
+        #self.buffer = header + data
+        #self.create_checksum()
+        print(type(data))
+        headdata = header.decode("utf-8")+data
+        my_checksum = PingPacket.create_checksum(headdata)
 
         # Now that we have the right checksum, we put that in. It's just easier
         # to make up a new header than to stuff it into the dummy.
-        header = struct.pack(
-            "BbHHh", icmp_type, 0, socket.htons(my_checksum), self.packet_seq, 1
-        )
-        self.packet = header + data
-
-    def create_checksum(self):
+        header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0,
+                             socket.htons(my_checksum), 1, 1)
+        self.packet = header + bytes(data, 'utf-8')
 
 
-        sum = 0
-        count_to = (len(self.buffer) / 2) * 2
-        count = 0
-
-        while count < count_to:
-            this_val = self.buffer[count + 1] * 256 + self.buffer[count]
-            sum += this_val
-            sum &= 0xffffffff  # Necessary?
-            count += 2
-
-        if count_to < len(self.buffer):
-            sum += self.buffer[len(self.buffer) - 1]
-            sum &= 0xffffffff  # Necessary?
-
-        sum = (sum >> 16) + (sum & 0xffff)
-        sum += sum >> 16
-        answer = ~sum
-        answer &= 0xffff
-
-        # Swap bytes. Bugger me if I know why.
-        answer = answer >> 8 | (answer << 8 & 0xff00)
-        self.checksum = answer
 
 
 class PingSocket(object):
@@ -96,8 +104,25 @@ class PingSocket(object):
 
     async def sendto_socket(self, dest_addr, id_, timeout, family):
         future = asyncio.get_event_loop().create_future()
-        packet = PingPacket(packet_seq=255)
+        packet = PingPacket(packet_seq=1)
         callback = functools.partial(self.socket_sendto, packet=packet.packet, dest_ip=dest_addr, future=future)
+        asyncio.get_event_loop().add_writer(self.socket, callback)
+        await future
+
+    def socket_receieve(self, future):
+        while True:
+
+            r = select.select([self.socket], [], [])
+            for i in r:
+                print("dd")
+                print(i), i.recvfrom(0)
+
+        asyncio.get_event_loop().remove_writer(self.socket)
+        future.set_result(None)
+
+    async def receive_socket(self):
+        future = asyncio.get_event_loop().create_future()
+        callback = functools.partial(self.socket_receieve,future=future)
         asyncio.get_event_loop().add_writer(self.socket, callback)
         await future
 
@@ -123,12 +148,42 @@ class PingClient(PingApp):
         print(my_id)
 
         await self.socket.sendto_socket(addr, my_id, self.timeout, family)
+        await self.recv()
         self.socket.socket.close()
 
+
     async def recv(self):
-        print("")
+        loop = asyncio.get_event_loop()
+        timeout = 10
+        try:
+            with async_timeout.timeout(timeout):
+                while True:
+                    rec_packet = await loop.sock_recv(self.socket.socket, 1024)
+                    time_received = default_timer()
+
+                    if self.socket.socket.family == socket.AddressFamily.AF_INET:
+                        offset = 20
+                    else:
+                        offset = 0
+
+                    icmp_header = rec_packet[offset:offset + 8]
 
 
+
+                    type, code, checksum, packet_id, sequence = struct.unpack(
+                        "bbHHh", icmp_header
+                    )
+                    data = rec_packet[offset + 8:offset + 8 + struct.calcsize("d")]
+                    #time_sent = struct.unpack("d", data)[0]
+                    #print(time_sent)
+
+                    print(rec_packet)
+                    if packet_id == 256:
+
+                        return time_received
+
+        except asyncio.TimeoutError:
+            raise TimeoutError("Ping timeout")
 class PingServer(PingApp):
     def __init__(self):
         super(PingServer, self).__init__()
@@ -157,11 +212,11 @@ class PingModeClient(PingMode):
     def init(self):
         # loop.run_until_complete(client.send("192.168.0.1"))
         # loop.create_task(some_coroutine())
-        self.loop.create_task(PingClient().send("192.168.0.1"))
-        self.loop.create_task(PingClient().send("192.168.0.1"))
+        self.loop.create_task(PingClient().send('192.168.0.19'))
+        #self.loop.create_task(PingClient().send('192.168.0.1'))
         # loop.create_task(print("test"))
-        self.loop.create_task(PingClient().send("192.168.0.1"))
-
+        #self.loop.create_task(PingClient().send('192.168.0.1'))
+        #self.loop.create_task(PingClient().recv())
         # loop.create_task(ping("192.168.0.1", 10))
         # loop.create_task(ping("192.168.0.19", 10))
         # loop.create_task(some_coroutine2())
